5e46eda962370bb24d789512b55c8d66700192da
Stefano Gini <61376940+Haruno19@users.noreply.github.com>
2022-03-21 22:44:52 +0100

    uploading project files

---
+++ Collatz Conjecture/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Collatz_Conjecture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(3)))), ((int)(((byte)(38)))));
+            this.chart1.BorderlineWidth = 0;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(213, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Light;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.IsVisibleInLegend = false;
+            series4.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series4.LabelForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            series4.Name = "Series1";
+            series4.SmartLabelStyle.Enabled = false;
+            series4.YValuesPerPoint = 2;
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series5.MarkerBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            series5.MarkerColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(120)))));
+            series5.MarkerSize = 10;
+            series5.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series5.Name = "Series2";
+            series6.ChartArea = "ChartArea1";
+            series6.LabelForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            series6.MarkerBorderColor = System.Drawing.Color.RoyalBlue;
+            series6.MarkerColor = System.Drawing.Color.RoyalBlue;
+            series6.Name = "Series3";
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(976, 608);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(23)))), ((int)(((byte)(58)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(26, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 13);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(23)))), ((int)(((byte)(58)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(26, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Single Chart";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(23)))), ((int)(((byte)(58)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.Location = new System.Drawing.Point(26, 128);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 417);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(23)))), ((int)(((byte)(58)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(109, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "First Digit Dist.";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(23, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Insert a number greater than zero";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(23)))), ((int)(((byte)(58)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(26, 580);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Clear Output";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Location = new System.Drawing.Point(130, 580);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(77, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "STOP";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(26, 551);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(181, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(23)))), ((int)(((byte)(58)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.Location = new System.Drawing.Point(26, 99);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(181, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Loop Count Dist.";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(3)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(1201, 632);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Collatz Conjecture";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Collatz Conjecture/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Collatz_Conjecture
+{
+    //O: 3x+1
+    //E: x/2
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GraphSetup();
+        }
+        long[] digits;
+        long currentState = 0;
+        Thread thread;
+        List<int> loops;
+
+        //------------ BUTTONS --------------//
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long max = Convert.ToInt64(textBox1.Text);
+            if (max > 0)
+                CalculateSequence(max);
+            else
+                MessageBox.Show("Insert a number greater than zero!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            long max = Convert.ToInt64(textBox1.Text);
+            if (max > 0)
+            {
+                InitializeGraph();
+                thread = new Thread(() => CalculateFirstDigitDistribution(max));
+                thread.Start();
+                timer1.Start();
+                progressBar1.Maximum = (int)max+1;
+            }
+            else
+                MessageBox.Show("Insert a number greater than zero!");
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            long max = Convert.ToInt64(textBox1.Text);
+            if (max > 0)
+            {
+                InitializeGraph();
+                /*thread = new Thread(() => CalculateLoopCountDistribution(max));
+                thread.Start();*/
+                CalculateLoopCountDistribution(max);
+                timer1.Start();
+                progressBar1.Maximum = (int)max + 1;
+            }
+            else
+                MessageBox.Show("Insert a number greater than zero!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            InitializeGraph();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (thread.IsAlive)
+                thread.Abort();
+        }
+
+        //------------ TIMERS --------------//
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateDigitChart();
+            textBox2.Text = string.Join(", ", digits);
+            progressBar1.Value = (int)currentState;
+            if (!thread.IsAlive)
+                timer1.Stop();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            UpdateLoopChart();
+            progressBar1.Value = (int)currentState;
+            if (!thread.IsAlive)
+                timer1.Stop();
+        }
+
+        //------------ FUNCTIONS --------------//
+
+        private void CalculateLoopCountDistribution(long max)
+        {
+            loops = new List<int>();
+
+            for (long k = 1; k <= max; k++)
+            {
+                currentState = k;
+                long i = k;
+                int l = 0;
+                do
+                {
+                    l++;
+                    i = (i % 2 == 0) ? i / 2 : i * 3 + 1;
+                } while (i != 1);
+                
+                
+            }
+        }
+
+        private void CalculateFirstDigitDistribution(long max)
+        {
+            digits = new long[] {0,0,0,0,0,0,0,0,0};
+
+            for (long k = 1; k <= max; k++)
+            {
+                currentState = k;
+                long i=k;
+                digits[(int)(i.ToString()[0]) - 48 - 1]++;
+                do
+                {
+                    i = (i % 2 == 0) ? i/2 : i * 3 + 1;
+                    digits[(int)(i.ToString()[0]) - 48 - 1]++;
+                } while (i != 1);
+            }
+        }
+
+        private void CalculateSequence(long max)
+        {
+            InitializeGraph();
+            string sequence = max.ToString() + "; ";
+            chart1.Series[0].Points.AddY(max);
+            chart1.Series[1].Points.AddY(max);
+            do
+            {
+                max = (max % 2 == 0) ? max / 2 : max * 3 + 1;
+                sequence += max.ToString() + "; ";
+                chart1.Series[0].Points.AddY(max);
+                chart1.Series[1].Points.AddY(max);
+            } while (max != 1);
+            textBox2.Text = sequence;
+        }
+
+        private void InitializeGraph()
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[2].Points.Clear();
+            textBox2.Text = "";
+            progressBar1.Maximum = 0;
+            currentState = 0;
+        }
+
+        private void GraphSetup()
+        {
+            chart1.Series[0].IsValueShownAsLabel = true;
+            chart1.ChartAreas["ChartArea1"].BackColor = Color.FromArgb(0, 3, 38);
+            chart1.ChartAreas["ChartArea1"].AxisX.LineColor = Color.FromArgb(120, 255, 255, 255);
+            chart1.ChartAreas["ChartArea1"].AxisY.LineColor = Color.FromArgb(120, 255, 255, 255);
+            chart1.ChartAreas["ChartArea1"].AxisX.LabelStyle.ForeColor = Color.FromArgb(120, 255, 255, 255);
+            chart1.ChartAreas["ChartArea1"].AxisY.LabelStyle.ForeColor = Color.FromArgb(120, 255, 255, 255);
+            chart1.ChartAreas["ChartArea1"].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas["ChartArea1"].AxisY.MajorGrid.Enabled = false;
+        }
+
+        private void UpdateDigitChart()
+        {
+            chart1.Series[2].Points.Clear();
+            for (int i = 0; i < digits.Length; i++)
+            {
+                chart1.Series[2].Points.AddXY(i + 1, digits[i]);
+            }
+        }
+
+        private void UpdateLoopChart()
+        {
+            /*chart1.Series[2].Points.Clear();
+            for (int i = 0; i < digits.Length; i++)
+            {
+                chart1.Series[2].Points.AddXY(i + 1, digits[i]);
+            }*/
+
+
+            textBox2.Text = loops.ToString();
+        }
+    }
+}
